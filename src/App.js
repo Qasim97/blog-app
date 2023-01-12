9b7cec6661128b05be29d811302b9f9f879eaa8e
Qasim97 <qasimjamal97@gmail.com>
2023-01-12 15:08:58 +0500

    write post component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import TopBar from "./components/topbar/Topbar";
-import Home from "./pages/home/Home"
+// import Home from "./pages/home/Home"
 // import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <>
         <TopBar></TopBar>
-        <Home></Home>
-        {/* <Single></Single> */}
+        <Write></Write>
         </>
       </header>
     </div>
