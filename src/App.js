bc52c6aa111cfc8fdd796ecc717bc1868d9e0c26
Qasim97 <qasimjamal97@gmail.com>
2023-01-13 21:28:19 +0500

    login and register pages are added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import TopBar from "./components/topbar/Topbar";
-// import Home from "./pages/home/Home"
+// import Login from "./pages/login/Login";
+// import Settings from "./pages/settings/Settings";
+import Home from "./pages/home/Home"
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
+// import Write from "./pages/write/Write";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+import Register from "./pages/register/Register";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <>
-        <TopBar></TopBar>
-        <Write></Write>
-        </>
-      </header>
+      <TopBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
